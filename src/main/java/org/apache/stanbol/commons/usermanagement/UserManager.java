edfbb6ce529074d57220b348a6feb18a00e40b9a
Reto Bachmann-Gmür <reto@apache.org>
2012-09-27 16:47:06 +0200

    ISSUE #1: added method to replace subgraph

--- src/main/java/org/apache/stanbol/commons/usermanagement/UserManager.java
+++ src/main/java/org/apache/stanbol/commons/usermanagement/UserManager.java
@@ -1,15 +1,23 @@
 package org.apache.stanbol.commons.usermanagement;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.lang.annotation.Annotation;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 import java.util.concurrent.locks.Lock;
 
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.ext.MessageBodyReader;
 
 import org.apache.clerezza.platform.config.SystemConfig;
+import org.apache.clerezza.rdf.core.Graph;
 import org.apache.clerezza.rdf.core.Language;
 import org.apache.clerezza.rdf.core.MGraph;
 import org.apache.clerezza.rdf.core.Triple;
@@ -17,23 +25,35 @@ import org.apache.clerezza.rdf.core.UriRef;
 import org.apache.clerezza.rdf.core.access.LockableMGraph;
 import org.apache.clerezza.rdf.core.impl.PlainLiteralImpl;
 import org.apache.clerezza.rdf.core.impl.SimpleMGraph;
+import org.apache.clerezza.rdf.core.serializedform.Parser;
+import org.apache.clerezza.rdf.core.serializedform.SupportedFormat;
 import org.apache.clerezza.rdf.ontologies.FOAF;
 import org.apache.clerezza.rdf.ontologies.PLATFORM;
 import org.apache.clerezza.rdf.ontologies.RDF;
 import org.apache.clerezza.rdf.ontologies.RDFS;
 import org.apache.clerezza.rdf.utils.GraphNode;
+import org.apache.clerezza.rdf.utils.MGraphUtils;
 import org.apache.clerezza.rdf.utils.UnionMGraph;
+import org.apache.clerezza.rdf.utils.MGraphUtils.NoSuchSubGraphException;
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.Service;
 import org.apache.stanbol.commons.ldviewable.LdViewable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Component
 @Service(UserManager.class)
 @Path("user-management")
 public class UserManager {
+	
+	private static Logger log = LoggerFactory.getLogger(UserManager.class);
+	
 	@Reference(target = SystemConfig.SYSTEM_GRAPH_FILTER)
 	private LockableMGraph systemGraph;
+	
+	@Reference
+	private Parser parser;
 	
 	@GET
 	public String index() {
@@ -73,6 +93,40 @@ public class UserManager {
 	public LdViewable viewUser(@QueryParam("userName") String userName) {
 		return new LdViewable("EditableUser.ftl", getUser(userName), this.getClass());
 	}
+	
+	/**
+	 * replaces the subgraph serialized with RDF/XML in <code>revokedString
+	 * </code> with the one from <code>assertedString</code>.
+	 *
+	 * @param graphUri the graph within which the replacement has to take place or null
+	 * for the content graph
+	 * @param assertedString the asserted Graph as RDF/XML
+	 * @param revokedString the revoked Graph as RDF/XML
+	 */
+	@POST
+	@Path("replace-subgraph")
+	public void replaceSubGraph(@QueryParam("graph") UriRef graphUri,
+			@FormParam("assert") String assertedString,
+			@FormParam("revoke") String revokedString) {
+		final Graph assertedGraph;
+		final Graph revokedGraph;
+		try {
+			assertedGraph = parser.parse(new ByteArrayInputStream(assertedString.getBytes("utf-8")), 
+					SupportedFormat.RDF_XML);
+			revokedGraph = parser.parse(new ByteArrayInputStream(assertedString.getBytes("utf-8")), 
+					SupportedFormat.RDF_XML);
+		} catch (IOException ex) {
+			log.error("reading graph {}", ex);
+			throw new WebApplicationException(ex, 500);
+		}
+		try {
+			MGraphUtils.removeSubGraph(systemGraph, revokedGraph);
+		} catch (NoSuchSubGraphException ex) {
+			throw new RuntimeException(ex);
+		}
+		systemGraph.addAll(assertedGraph);
+	}
+
 	
 	private GraphNode getUser(@QueryParam("userName") String userName) {
 		Iterator<Triple> iter = systemGraph.filter(null, PLATFORM.userName, new PlainLiteralImpl(userName));
